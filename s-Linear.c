ee86ad992cc5543aba1832e241dd82d99e479fc4
is1807372 <is1807372@qu.edu.qa>
2026-02-20 13:46:47 +0300

    binary

---
+++ s-Linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (void) {
+    int arr[] = {1,3,5,7,8,9,10,11,12,13};
+    int number;
+    int arrsize = sizeof(arr)/sizeof(arr[0]);
+    printf("Enter a number to start the binary search: ");
+    scanf("%d", &number); 
+    int i = arrsize/2;
+    
+    if (arr[i]==number){
+        printf("Yes it here");}
+        else if (arr[i]>number){
+            i = 
+    
+         
+    }
+        
+    
+
+
+
+    
+
+}
